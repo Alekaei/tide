93266ae26e3224f5e1849ee15ca91971c5d656e9
Alekaei <alekaei522@gmail.com>
2018-10-14 23:03:18 +0100

    Added file rendering

--- tide_editor/classes/File.py
+++ tide_editor/classes/File.py
@@ -1,12 +1,21 @@
+import os
+
 class File:
 	def __init__(self, path):
 		self.needs_saving = False
+		self.name = os.path.basename(path);
 		self.lines = [line.rstrip('\n') for line in open(path, 'r')]
+
+		self.encoding = 'utf-8'
+		self.fileType = 'python'
 
 		self.cursor_x = 0
 		self.cursor_y = 0
 		self.far_x = 0
 		self.far_y = 0
+
+	def getLowerLimit(self):
+		return 1
 
 	def Save(self):
 		self.needs_saving = False
